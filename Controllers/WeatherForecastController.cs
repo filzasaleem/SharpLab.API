ebace9facc146d7d3a271aeb2a8543304492db24
Filza Saleem <filza.saleem05@gmail.com>
2025-11-13 21:50:35 +0100

    first commit

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace SharpLabs.API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    
+}
